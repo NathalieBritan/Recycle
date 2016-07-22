4cad3854c78c875513b9027293acd53b01a9fabd
Ruslan Leshchenko <ruslan.leshchenko@ekreative.com>
2016-07-22 16:46:34 +0300

    all orientation

--- app/src/main/java/com/example/ruslan/recyclelist/Activity/MainActivity.java
+++ app/src/main/java/com/example/ruslan/recyclelist/Activity/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.ruslan.recyclelist.Activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.example.ruslan.recyclelist.Adapter.RecycleAdapter;
@@ -30,7 +31,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView=(RecyclerView)findViewById(R.id.recycle_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        if(getResources().getConfiguration().orientation== Configuration.ORIENTATION_PORTRAIT){
+            recyclerView.setLayoutManager(new GridLayoutManager(this,1));
+        }
+        else
+        {
+            recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        }
+
+
         recyclerView.setAdapter(new RecycleAdapter(getSampleArrayList()));
     }
 }
